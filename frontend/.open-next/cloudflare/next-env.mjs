05427ffebb716e4261ba83a6a7686c3bbadd657b
vishalingole <vishal.ingol3@gmail.com>
2026-08-01 15:30:30 +0530

    changes

---
+++ frontend/.open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_API_URL":"http://localhost:3001/api/v1","NEXT_PUBLIC_SOCKET_URL":"http://localhost:3001"};
+export const development = {"NEXT_PUBLIC_API_URL":"http://localhost:3001/api/v1","NEXT_PUBLIC_SOCKET_URL":"http://localhost:3001"};
+export const test = {};
---
+++ frontend/scripts/fix-cloudflare-pages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp .open-next/worker.js .open-next/_worker.js
+cp -r .open-next/assets/* .open-next/
